f9acfbe4f693214e4d2dda8be4e27f325b86815d
Aditya Raj Singh <207472164+Aditya-hub1@users.noreply.github.com>
2026-06-07 10:20:40 +0530

    Add files via upload

---
+++ Array/left_rotate_by1.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void rotate_left(vector<int>& arr) {
+    int n = arr.size();
+
+    int temp = arr[0];
+    int k;
+    cout<<"entter K::";
+    cin>>k;
+    for(int j=0;j<k;j++){
+        int temp=arr[0];
+    
+
+    for(int i = 1; i < n; i++) {
+        arr[i - 1] = arr[i];
+    }
+
+    arr[n - 1] = temp;
+}}
+
+int main() {
+    int n;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    rotate_left(arr);  
+
+    cout << "Array after left rotation: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
